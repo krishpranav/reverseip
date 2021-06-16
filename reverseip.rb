8c10a23dc53651e2ef185e3847ea740a444c0e01
krishpranav <krisna.pranav@gmail.com>
2021-06-16 08:19:13 +0530

    [+] else function [+]

--- reverseip.rb
+++ reverseip.rb
@@ -45,3 +45,20 @@ end
 
 if parsed['status'] == 'failure' || parsed['status'] == 'failure'
 	abort('[!] '.colorize(:red) + parsed['message'].split('. ')[0] + "\n#{sep}\n")
+else
+	puts 'Target            -- '.colorize(:red) + parsed['remoteIpAddress']
+	puts 'Number of Domains -- '.colorize(:red) + parsed['domainCount']; len = parsed['domainCount'].length
+	puts sep
+
+	domains = parsed['domainArray']
+	puts 'Domains -- '.colorize(:red)
+	domains.each.with_index(1) {|domain, index|
+		dashes     = '-' * (domains.flatten.max_by(&:size).length - domain[0].length + 1) + ' '
+		httpStatus = statusCode(domain[0])
+		
+		print '[ '.colorize(:cyan) + index.to_s.rjust(len, '0').colorize(:red) + ' ]'.colorize(:cyan) + ' -- '
+		print '[ '.colorize(:cyan) + domain[0].to_s.colorize(:green) + ' ] '.colorize(:cyan) + dashes.to_s
+		print '[ '.colorize(:cyan) + httpStatus.to_s + ' ]'.colorize(:cyan)
+		puts getCMS(domain[0])
+	}
+end
